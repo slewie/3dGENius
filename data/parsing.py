90bbce31557acca4de34658e85da3371d4bb46c1
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2023-10-09 10:46:39 +0300

    finish adjacency matrix algorithm

--- data/parsing.py
+++ data/parsing.py
@@ -10,36 +10,40 @@ from matplotlib import pyplot
 # Load the STL file.
 my_mesh = mesh.Mesh.from_file('data/cube.stl')
 
-# print(my_mesh.vectors)
-
 # Initialise adjacency matrix with shape of NUM_VERTICES ** 2.
 vertices_num = len(my_mesh.points) * 3
 adjacency_matrix = np.zeros(
     shape=(vertices_num, len(my_mesh.points) * 3), dtype=int)
+print("Adjacency matrix shape: ", adjacency_matrix.shape)
 
 # Go through each vertex and check
 # O(n ** 2) complexity to build the adjacency matrix.
 for i in range(len(my_mesh.vectors)):
-    triangle1 = my_mesh.vectors[i]
+    triangle1 = np.array(my_mesh.vectors[i])
 
     for j in range(len(triangle1)):
-        triangle1_vertex = triangle1[j]
+        triangle1_vertex = np.array(triangle1[j])
 
         for k in range(len(my_mesh.vectors)):
-            triangle2 = my_mesh.vectors[j]
+            triangle2 = np.array(my_mesh.vectors[k])
+            is_adjaced = False
+
+            # Check if this vertex is within the same triangle with other vertex.
+            for triangle2_vertex in triangle2:
+                if (triangle1_vertex == triangle2_vertex).all():
+                    is_adjaced = True
+                    break
 
             for z in range(len(triangle2)):
-                triangle2_vertex = triangle2[z]
+                triangle2_vertex = np.array(triangle2[z])
 
                 # TODO: Sanity check for float comparison.
 
                 # Do not add the loop connection.
                 if (triangle1_vertex == triangle2_vertex).all():
                     continue
-
-                # Check if this vertex is within the same triangle with other vertex.
-                if triangle1_vertex in triangle2:
-                    adjacency_matrix[j][z] = 1
+                elif is_adjaced:
+                    adjacency_matrix[i * 3 + j][k * 3 + z] = 1
 
 for row in adjacency_matrix:
     print(row)
